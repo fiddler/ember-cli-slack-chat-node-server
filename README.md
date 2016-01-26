d6820caa577bdde741df65226b0cc27b5dda3f36
Jussi Kaijalainen <fiddler@iki.fi>
2016-01-26 17:31:10 +0200

    Initial code & short documentation

---
+++ README.md
@@ -0,0 +1,14 @@
+# Ember-cli-slack-chat-node-server
+
+This is a simple node.js server component to go with [Ember-cli-slack-chat](https://github.com/fiddler/ember-cli-slack-chat).
+
+## Installation
+
+* You have to setup a Slack API key to access the API. You can do that at [https://api.slack.com/web](https://api.slack.com/web)
+* Setup the API key as a Environment variable named SLACK_API_TOKEN or set it up in the server.js.
+* Point your Ember-cli-slack-chat to the the server.
+
+## Considerations
+
+This is just a quick example and allows any origin for requests, so edit to your needs.
+
---
+++ server.js
@@ -0,0 +1,64 @@
+var Slack = require('slack-node');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// Setup Slack API
+var slackApiToken = process.env.SLACK_API_TOKEN || 'your Slack API token here';
+var slack = new Slack(slackApiToken);
+
+
+// Setup Express
+var app = express();
+var server = app.listen( 1337, function() {
+  console.log('Express listening at http://%s:%s', server.address().address, server.address().port);
+});
+
+// Express app configs
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Allow crossdomain
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
+});
+
+// Query messages
+app.get('/channel/:id/messages', function(req, res, next) {
+  if( req.params.id ) {
+    slack.api('channels.history', {
+      channel: req.params.id,
+      oldest: req.query.latest
+    }, function(err, response) {
+      res.send(response);
+    });
+  } else {
+    res.send({ error : 'Data missing'});
+  }
+});
+
+// Create channel
+app.post('/channels', function(req, res, next) {
+  slack.api('channels.create', {
+    name: req.body.channel
+  }, function(err, response) {
+    res.send(response);
+  });
+});
+
+// Send message
+app.post('/message', function(req, res, next) {
+  if( req.body.message && req.body.channel ){
+    slack.api('chat.postMessage', {
+      text:req.body.message,
+      channel: req.body.channel,
+      attachment: req.body.attachment
+    }, function(err, postMessageResponse){
+      res.send(postMessageResponse);
+    });
+  } else{
+    res.send({ error : 'Data missing'});
+  }
+});
